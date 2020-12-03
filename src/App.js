d3600cc0ff0a9a20499d0651f60d6cf2db9f48eb
David <david.krtolica8@gmail.com>
2020-12-03 12:04:28 +0100

    Added bootstrap to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 
 import AddBike from "./components/bikes/add-bike.component";
